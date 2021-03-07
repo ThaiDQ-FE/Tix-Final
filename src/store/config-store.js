400922275b2caccf9ad4d9583984497b21183d7d
ThaiChubby <dangquocthai07061998@gmail.com>
2021-03-08 01:04:11 +0700

    Use Thunk

--- src/store/config-store.js
+++ src/store/config-store.js
@@ -1,6 +1,12 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers/root.reducer";
+import thunk from "redux-thunk";
 
-const store = createStore(rootReducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 export default store;

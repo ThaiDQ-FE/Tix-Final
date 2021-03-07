12055f03978df0f23325b82b1ee10875d154e05f
ThaiChubby <dangquocthai07061998@gmail.com>
2021-03-08 01:12:05 +0700

    Use Thunk

--- src/store/actions/movie.action.js
+++ src/store/actions/movie.action.js
@@ -1,5 +1,8 @@
 import axios from "axios";
-import { GET_MOVIE_LIST } from "../constants/movie.const";
+import {
+  GET_MOVIE_LIST_SUCCESS,
+  GET_MOVIE_LIST_FAILED,
+} from "../constants/movie.const";
 
 export const getMovieList = () => {
   return (dispatch) => {
@@ -10,8 +13,24 @@ export const getMovieList = () => {
       data: null,
     })
       .then((res) => {
-        console.log(res.data);
+        dispatch(getMovieListSuccess(res.data));
       })
-      .catch((err) => {});
+      .catch((err) => {
+        dispatch(getMovieListFailed(err));
+      });
+  };
+};
+
+const getMovieListSuccess = (movieList) => {
+  return {
+    type: GET_MOVIE_LIST_SUCCESS,
+    payload: movieList,
+  };
+};
+
+const getMovieListFailed = (err) => {
+  return {
+    type: GET_MOVIE_LIST_FAILED,
+    payload: err,
   };
 };
--- src/store/reducers/movie.reducer.js
+++ src/store/reducers/movie.reducer.js
@@ -1,14 +1,25 @@
-import { GET_MOVIE_LIST } from "../constants/movie.const";
+import {
+  GET_MOVIE_LIST,
+  GET_MOVIE_LIST_FAILED,
+  GET_MOVIE_LIST_SUCCESS,
+} from "../constants/movie.const";
 
 const initialState = {
   movieList: [],
+  errors: {},
 };
 
 const movieReducer = (state = initialState, action) => {
   const { type, payload } = action;
   switch (type) {
     case GET_MOVIE_LIST: {
+    }
+    case GET_MOVIE_LIST_SUCCESS: {
       state.movieList = payload;
+      return { ...state };
+    }
+    case GET_MOVIE_LIST_FAILED: {
+      state.errors = payload;
       return { ...state };
     }
     default:

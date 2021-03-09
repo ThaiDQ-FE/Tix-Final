bffb834c1c05b4ad7ca810a1c40f288a228f913c
ThaiChubby <dangquocthai07061998@gmail.com>
2021-03-09 13:34:59 +0700

    Using router + create booking pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
 import Home from "./pages/user/home";
 import DetailMovie from "./pages/user/detail-movie";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <>
-      {/* <Home /> */}
-      <DetailMovie />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact={true}>
+            <Home />
+          </Route>
+          <Route path="/movie-detail">
+            <DetailMovie />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }

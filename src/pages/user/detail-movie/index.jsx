903e92dcd439fd4ce205ee14a17930e4c0015aed
ThaiChubby <dangquocthai07061998@gmail.com>
2021-03-09 13:30:34 +0700

    Using sass + install lib react-router-dom

--- src/pages/user/detail-movie/index.jsx
+++ src/pages/user/detail-movie/index.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { getMovieDetail } from "../../../store/actions/movie.action";
 import { connect } from "react-redux";
+import "./styles.scss";
 
 class DetailMovie extends Component {
   render() {
     return (
       <div>
+        <h2 className="test">Detail Movie</h2>
         <img src={this.props.movieDetail.hinhAnh} />
       </div>
     );

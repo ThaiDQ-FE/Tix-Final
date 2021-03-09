43800d5d0b5f542f9ff7c6851833acaba74d24e8
ThaiChubby <dangquocthai07061998@gmail.com>
2021-03-09 13:54:16 +0700

    Componnent Header

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import "./App.css";
 import Home from "./pages/user/home";
 import DetailMovie from "./pages/user/detail-movie";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Booking from "./pages/user/booking";
+import Header from "./components/header";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Switch>
           <Route path="/" exact={true}>
             <Home />
           </Route>
           <Route path="/movie-detail">
             <DetailMovie />
+          </Route>
+          <Route path="/booking">
+            <Booking />
           </Route>
         </Switch>
       </BrowserRouter>
---
+++ src/pages/user/booking/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Booking() {
+  return (
+    <div>
+      <h2>Booking</h2>
+    </div>
+  );
+}
+export default Booking;
